a4c174c744d717e9e8b62582e45ea7964ce67a2e
toju55 <desgrieux.tj@googlemail.com>
2023-11-15 18:15:15 +0100

    Init Spring Boot project

---
+++ src/test/java/de/toju/vocatrain/VocabularyTrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.toju.vocatrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocabularyTrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
